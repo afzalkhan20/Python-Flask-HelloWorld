994fe187545841ba946faec1f9001c0418ad8c19
afzalkhan20 <afzalkhan20031989@gmail.com>
2021-01-25 15:22:54 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application! Is it really working ??'
 
-if __name__ == '__main__':
-  app.run()
+@app.route("/")
+def home():
+    return "Hello, this is a sample Python Web App running on Flask Framework!"
