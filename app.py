6e95c71b2d6fac9ece79d633083b0f096a560c5b
afzalkhan20 <afzalkhan20031989@gmail.com>
2021-01-28 09:02:19 +0400

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def preprocessing(df_preprocess):
 
 
 def categorize(description):
-    logging.info('Entry to categorize')
+    logging.info('Entry to categorize function')
     des_kwrds = set(description.split(" "))
 
     vehicle_cat_prob = len(des_kwrds.intersection(vehicle_keywords)) / vehicle_kwrds_len
